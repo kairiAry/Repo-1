dbe1c8b5c5e7af53f3f3e2bac401be62eb0208fd
kairiAry <hairiamrullah@gmail.com>
2024-09-21 01:21:14 +0800

    first commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
+window.onload = function() {
+    document.getElementById('content').style.display = "none";
+    Alert();
+}
+
 function Alert() {
     namaPengguna = prompt("Hai! Silakan masukkan nama Anda:");
-
+    
     document.getElementById('nama').innerHTML = namaPengguna;
     document.getElementById('content').style.display = "block";
 }
